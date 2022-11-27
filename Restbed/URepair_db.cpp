3331c0c292b0013d976d914c5175eaf15cdc8968
jordanvalenzuela <113567393+jordanvalenzuela@users.noreply.github.com>
2022-11-26 19:31:01 -0800

    Update URepair_db.cpp

--- Restbed/URepair_db.cpp
+++ Restbed/URepair_db.cpp
@@ -10,6 +10,7 @@ namespace URepairdb{
 
 std::mutex g_dbMutex;
 using JobDb = std::vector<Job>;
+using UserDb = std::vector<User>;
 using SessionPtr = std::shared_ptr<restbed::Session>;  
 
 class CustomLogger : public restbed::Logger
@@ -30,8 +31,7 @@ class CustomLogger : public restbed::Logger
             va_end(arguments);
         }
 
-        void log_if(bool expression, const Level level, const char* format,
-            ...) override
+        void log_if(bool expression, const Level level, const char* format, ...) override
         {
             if (expression)
             {
@@ -43,24 +43,41 @@ class CustomLogger : public restbed::Logger
         }
     };
   
-JobDb load()
+JobDb job_load()
     {
         JobDb db;
         db.emplace_back(fromJson(
-            R"json({"title":"","desc":"","location":"","status":1,"price":150,"time":})json"));
+            R"json({"title":"","desc":"","location":"","status":1,"price":150,"time":0})json"));
         {
-            Comic comic;
-            comic.title = "The Fantastic Four";
-            comic.desc = "";
-            comic.location = "";
-            comic.status = 1;
-            comic.price = 150;
-            comic.time = ;
+            Job job;
+            job.title = "The Fantastic Four";
+            job.desc = "";
+            job.location = "";
+            job.status = 1;
+            job.price = 150;
+            job.time = 0;
             db.push_back(job);
         }
         return db;
     }  
-  
+    
+UserDb use_load()
+    {
+        UserDb u_db;
+        db.emplace_back(fromJson(R"json({"username":"John_Doe1234","name":"John Doe","address":"1234 Seymour Street, Kamloops, BC, A1C 2C3","email":john_doe@urepair.ca,"phone":2503161234,"type":"Customer"})json"));
+        {
+            Job job;
+            job.username = "John_Doe1234";
+            job.name = "John Doe";
+            job.address = "1234 Seymour Street, Kamloops, BC, A1C 2C3";
+            job.email = "john_doe@urepair.ca";
+            job.phone = "2503161234";
+            job.type = "Customer";
+            db.push_back(job);
+        }
+        return u_db;
+    }   
+    
 std::shared_ptr<restbed::Settings> getSettings()
 {
   auto settings = std::make_shared<restbed::Settings>();
@@ -109,7 +126,7 @@ void addJob(const SessionPtr& session, ComicDb& db){
      const std::string json{ reinterpret_cast<const char*>(data.data()), data.size() 
    };
    Job job = fromJson(json);
-   if (job.title.empty() || job.desc.empty() || job.location.empty() || job.status < 1 || job.status > 3 || job.price < 0 || job.time < 0 || job.time > 12 //or 24)
+   if (job.title.empty() || job.desc.empty() || job.location.empty() || job.status < 1 || job.status > 3 || job.price < 0 || job.time < 0 || job.time > 12 /*or 24*/)
         {
             notAcceptable(session, "Not Acceptable, invalid JSON");
             return;
@@ -143,8 +160,44 @@ void publishJob(restbed::Service& service, JobDb& db)
         createJobCallback->set_method_handler("POST", createJobCallback);
         service.publish(createJobCallback);
     }                 
-                 
-void viewJob(const SessionPtr& session, ComicDb& db){
+
+void updateJob(const SessionPtr& session, JobDb& db)
+    {
+        std::size_t id{};
+        if (!validId(session, db, id))
+            return;
+
+        auto& request = session->get_request();
+        std::size_t length{};
+        length = request->get_header("Content-Length", length);
+        if (length == 0)
+        {
+            notAcceptable(session, "Not Acceptable, empty request body");
+            return;
+        }
+
+        session->fetch(
+            length,
+            [&db, id](const SessionPtr& session, const restbed::Bytes& data)
+            {
+                const std::string json{ reinterpret_cast<const char*>(data.data()), data.size() 
+            };
+        Job job = fromJson(json);
+        if (job.title.empty() || job.desc.empty() || job.location.empty() || job.status < 1 || job.status > 3 || job.price < 0 || job.time < 0 || job.time > 12 /*or 24*/)
+        {
+            notAcceptable(session, "Not Acceptable, invalid JSON");
+            return;
+        }
+
+        {
+            std::unique_lock<std::mutex> lock(g_dbMutex);
+            db[id] = job;
+        }
+        session->close(restbed::OK);
+            });
+    }                 
+
+void viewJob(const SessionPtr& session, JobDb& db){
   std::size_t id{};
   if (validId(session, db, id))
   {
@@ -153,35 +206,98 @@ void viewJob(const SessionPtr& session, ComicDb& db){
   }
 }
 
-void deleteJob(const SessionPtr& session, ComicDb& db){
+void deleteJob(const SessionPtr& session, JobDb& db){
   std::size_t id{};
   if (validId(session, db, id))
   {
     {
       std::unique_lock<std::mutex> lock(g_dbMutex);
-      db[id] = Comic{};
+      db[id] = Job{};
     }
     session->close(restbed::OK);
   }
-}
+} //Job Information
 
   
-void addUser(){
-  
+void addUser(const SessionPtr& session, UserDb& u_db){
+  auto& request = session->get_request();
+  std::size_t length{};
+  length = request->get_header("Content-Length", length);
+  if (length == 0)
+  {
+    notAcceptable(session, "Not Acceptable, empty body");
+    return;
+  }
+
+  session->fetch(length, [&u_db](const SessionPtr& session, const restbed::Bytes& data)
+   {
+     const std::string json{ reinterpret_cast<const char*>(data.data()), data.size() 
+   };
+   User user = fromJson(json);
+   if (user.username.empty() || user.name.empty() || user.address.empty() || user.email.empty()|| user.phone.empty() || user.type.empty())
+        {
+            notAcceptable(session, "Not Acceptable, invalid JSON");
+            return;
+        }
+
+        {
+            std::unique_lock<std::mutex> lock(g_dbMutex);
+            u_db.push_back(user);
+        }
+        session->close(restbed::OK);
+    });
 }
 
-void editUser(){
-  
+void editUser(const SessionPtr& session, UserDb& u_db){
+  std::size_t id{};
+        if (!validId(session, u_db, id))
+            return;
+
+        auto& request = session->get_request();
+        std::size_t length{};
+        length = request->get_header("Content-Length", length);
+        if (length == 0)
+        {
+            notAcceptable(session, "Not Acceptable, empty request body");
+            return;
+        }
+
+        session->fetch(
+            length,
+            [&u_db, id](const SessionPtr& session, const restbed::Bytes& data)
+            {
+                const std::string json{ reinterpret_cast<const char*>(data.data()), data.size() };
+        User user = fromJson(json);
+        if (user.username.empty() || user.name.empty() || user.address.empty() || user.email.empty() || user.phone.empty() || user.type.empty())
+        {
+            notAcceptable(session, "Not Acceptable, invalid JSON");
+            return;
+        }
+
+        {
+            std::unique_lock<std::mutex> lock(g_dbMutex);
+            u_db[id] = user;
+        }
+        session->close(restbed::OK);
+            });
 }
 
-void deleteUser(){
-  
-}
+void deleteUser(const SessionPtr& session, UserDb& u_db){
+  std::size_t id{};
+  if (validId(session, u_db, id))
+  {
+    {
+      std::unique_lock<std::mutex> lock(g_dbMutex);
+      db[id] = User{};
+    }
+    session->close(restbed::OK);
+  }
+} //User Information
 
 void runService()
     {
-        JobDb db = load();
-
+        JobDb db = job_load();
+        UserDb u_db = use_load();
         restbed::Service job_service;
         publishResources(service, db);
         job_service.set_logger(std::make_shared<CustomLogger>());
@@ -193,7 +309,7 @@ void runService()
 int main(){
   try
     {
-        jobdb::runService();
+        URepairdb::runService();
     }
     catch (const std::exception& bang)
     {
