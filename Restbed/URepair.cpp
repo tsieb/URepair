6525660eb544bc48b164aba75f641b421beccd1c
jordanvalenzuela <113567393+jordanvalenzuela@users.noreply.github.com>
2022-11-26 18:52:51 -0800

    Update URepair.cpp

--- Restbed/URepair.cpp
+++ Restbed/URepair.cpp
@@ -21,9 +21,9 @@ namespace jobdb{
   addMember("title", job.title);
   addMember("desc", job.desc);
   addMember("location", job.location);
-  obj.AddMember("status", job., doc.GetAllocator());
-  obj.AddMember("price", job., doc.GetAllocator());
-  obj.AddMember("time", job., doc.GetAllocator());
+  obj.AddMember("status", job.status, doc.GetAllocator());
+  obj.AddMember("price", job.price, doc.GetAllocator());
+  obj.AddMember("time", job.time, doc.GetAllocator());
   
   rapidjson::StringBuffer buffer;
   rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
@@ -46,3 +46,43 @@ Job fromJson(const string& json){
 }
   
 } //namespace jobdb
+
+namespace userdb{
+  string toJson(const User& user){
+  rapidjson::Document use_doc;  
+  rapidjson::Value& obj = use_doc.SetObject();
+
+  auto addMember = [&obj, &use_doc](const char* key, const std::string& value)
+  {
+    using String = rapidjson::GenericStringRef<char>;
+    obj.AddMember(String{ key }, String{ value.c_str() }, use_doc.GetAllocator());
+  };
+  
+  addMember("username", user.username);
+  addMember("name", user.name);
+  addMember("address", user.address);
+  addMember("email", user.email);
+  addMember("phone", user.phone);
+  addMember("type", user.type);
+  
+  rapidjson::StringBuffer buffer;
+  rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+  doc.Accept(writer);
+  return buffer.GetString();
+  }
+  
+User fromJson(const string& json){
+ rapidjson::Document use_doc;
+ use_doc.Parse(json.cstr());
+ User user{};
+ auto getString = [&use_doc](const char* key) { return use_doc[key].GetString(); };
+ user.username = getString("title");
+ user.name = getString("desc");
+ user.address = getString;
+ user.email = getString;
+ user.phone = getString;
+ user.type = getString;
+ return user;
+}
+  
+} //namespace userdb
