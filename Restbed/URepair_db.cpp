2a8816c0f0bcb6f9188bd5f305899867a790898b
jordanvalenzuela <113567393+jordanvalenzuela@users.noreply.github.com>
2022-11-26 16:48:02 -0800

    Update URepair_db.cpp

--- Restbed/URepair_db.cpp
+++ Restbed/URepair_db.cpp
@@ -7,6 +7,28 @@
 #include <mutex>
 
 namespace URepairdb{
+  
+}
+
+bool validId(const SessionPtr &session, const JobDb &db, std::size_t &id)
+{
+    const auto &request = session->get_request();
+    if (request->has_path_parameter("id"))
+    {
+        id = request->get_path_parameter("id", 0);
+        if (id < db.size() && db[id].issue != Job::DELETED_ISSUE)
+        {
+            return true;
+        }
+        notAcceptable(session, "Not Acceptable, id out of range");
+    }
+    else
+    {
+        notAcceptable(session, "Not Acceptable, missing id");
+    }
+    return false;
+}
+
 void addUser(){
   
 }
@@ -31,7 +53,7 @@ void deleteJob(){
   
 }
   
-} // namespace URepairdb
+
 
 int main(){
   
